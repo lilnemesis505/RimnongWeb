a4cb7d94dddc1d2d1b1fe67f65a6b4792e31d17a
lilnemesis505 <nmewcople@gmail.com>
2025-11-01 18:43:14 +0700

    atock sidebar

--- resources/views/layouts/stock/stock.blade.php
+++ resources/views/layouts/stock/stock.blade.php
@@ -31,41 +31,8 @@
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-        <span class="navbar-brand">ข้อมูลล็อตสินค้า</span>
-    </nav>
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <a href="#" class="brand-link">
-            <span class="brand-text font-weight-light">{{ session('admin_fullname') }}</span>
-        </a>
-        <div class="sidebar">
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column">
-                    <li class="nav-item">
-                        <a href="{{ route('welcome') }}" class="nav-link text-white">
-                            <i class="nav-icon fas fa-home-alt"></i> <p>หน้าหลัก</p>
-                        </a>
-                    </li>
-                </ul>
-                <hr style="border-top: 1px solid #fff;">
-                <ul class="nav nav-pills nav-sidebar flex-column">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active" style="background-color: #007bff;">
-                            <i class="nav-icon fas fa-box"></i> <p>ข้อมูลล็อตสินค้า</p>
-                        </a>
-                    </li>
-                     <li class="nav-item">
-                        <a href="{{ route('stock.add') }}" class="nav-link text-white">
-                            <i class="nav-icon fas fa-plus"></i> <p>เพิ่มข้อมูลนำเข้า</p>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('withdraw.create') }}" class="nav-link"><i class="nav-icon fas fa-dolly-flatbed"></i> <p>เบิกวัตถุดิบ</p></a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </aside>
+@include('layouts.assets._navbar')
+   @include('layouts.assets._sidebar')
     <div class="content-wrapper p-3" style="min-height: 100vh;">
         <div class="card">
             <div class="card-header">
