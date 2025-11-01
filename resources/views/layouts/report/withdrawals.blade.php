d2c4b45f558e2b8505648ea09222a0b64e130b91
lilnemesis505 <nmewcople@gmail.com>
2025-11-01 18:55:41 +0700

    www

--- resources/views/layouts/report/withdrawals.blade.php
+++ resources/views/layouts/report/withdrawals.blade.php
@@ -9,84 +9,8 @@
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
     {{-- (Navbar และ Sidebar เหมือนเดิม - คัดลอกจากไฟล์ welcome.blade.php มาได้เลย) --}}
-    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-         {{-- (โค้ด Navbar...) --}}
-    </nav>
-   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <a href="{{ route('welcome') }}" class="brand-link">
-            <span class="brand-text font-weight-light">{{ session('admin_fullname') }}</span>
-        </a>
-        <div class="sidebar">
-            <nav class="mt-2">
-                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                    <li class="nav-item">
-                        <a href="{{ route('welcome') }}" class="nav-link">
-                            <i class="nav-icon fas fa-home-alt"></i> <p>หน้าหลัก</p>
-                        </a>
-                    </li>
-                    
-                    <li class="nav-header">การจัดการ</li>
-                    <li class="nav-item has-treeview">
-                        <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-cogs"></i>
-                            <p>จัดการข้อมูลระบบ <i class="right fas fa-angle-left"></i></p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item"><a href="{{ route('product.index') }}" class="nav-link"><i class="nav-icon fas fa-shopping-cart"></i><p>จัดการข้อมูลสินค้า</p></a></li>
-                            <li class="nav-item"><a href="{{ route('employee.index') }}" class="nav-link"><i class="nav-icon fas fa-user-tie"></i><p>จัดการข้อมูลพนักงาน</p></a></li>
-                            <li class="nav-item"><a href="{{ route('customer.index') }}" class="nav-link"><i class="nav-icon fas fa-users"></i><p>ข้อมูลลูกค้า</p></a></li>
-                            <li class="nav-item"><a href="{{ route('stock.index') }}" class="nav-link"><i class="nav-icon fas fa-boxes"></i><p>จัดการข้อมูลล็อตสินค้า</p></a></li>
-                            <li class="nav-item"><a href="{{ route('promotion.index') }}" class="nav-link"><i class="nav-icon fas fa-tags"></i><p>จัดการข้อมูลโปรโมชั่น</p></a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('history.index') }}" class="nav-link"><i class="nav-icon fas fa-history"></i> <p>ข้อมูลการสั่งซื้อสินค้า</p></a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{ route('withdraw.create') }}" class="nav-link"><i class="nav-icon fas fa-dolly-flatbed"></i> <p>เบิกวัตถุดิบ</p></a>
-                    </li>
-                    
-                     <li class="nav-header">รายงาน</li>
-                    <li class="nav-item has-treeview menu-open">
-                        <a href="#" class="nav-link active">
-                            <i class="nav-icon fas fa-chart-pie"></i> 
-                            <p>รายงาน <i class="right fas fa-angle-left"></i></p>
-                        </a>
-                        <ul class="nav nav-treeview">
-                            <li class="nav-item">
-                                <a href="{{ route('salereport.index') }}" class="nav-link">
-                                    <i class="fas fa-chart-line nav-icon text-teal"></i> 
-                                    <p>รายงานการขายสินค้า</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('report.bills') }}" class="nav-link ">
-                                    <i class="fas fa-chart-bar nav-icon "></i> 
-                                    <p>รายงานยอดขาย</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('report.withdrawals') }}" class="nav-link active">
-                                    <i class="fas fa-clipboard-list nav-icon"></i> 
-                                    <p>รายงานการเบิกวัตถุดิบ</p>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="{{ route('report.adjustments') }}" class="nav-link ">
-                                    <i class="fas fa-sliders-h nav-icon "></i> 
-                                    <p>รายงานกาปรับยอดล็อตสินค้า</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-header">อื่นๆ</li>
-                    <li class="nav-item">
-                        <a href="#" class="nav-link" data-toggle="modal" data-target="#logoutModal"><i class="nav-icon fas fa-sign-out-alt text-danger"></i> <p>ออกจากระบบ</p></a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </aside>
+   @include('layouts.assets._navbar')
+  @include('layouts.assets._sidebar')
     <div class="content-wrapper p-3">
         <section class="content-header">
             <div class="container-fluid">
